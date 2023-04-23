db1b617658acf7fb3c9fae214bb4a5788829ce98
oupengfei <66344669+oupengfei@users.noreply.github.com>
2023-04-23 13:44:51 +0800

    删除了默认页面显示的文字

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,4 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 本站功能强大，响应速度快，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
